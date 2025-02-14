5f64699121924632382c24388b358de3d41b3a04
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-02-14 12:24:24 +0100

    Update ConstantesApp.java

--- C2-JavaBasics/src/ConstantesApp.java
+++ C2-JavaBasics/src/ConstantesApp.java
@@ -7,6 +7,9 @@ public class ConstantesApp {
 //		IVA = 0.21; A las Constantes no se les puede reasignar valor
 		double resultado = producto * IVA;
 		System.out.println("El IVA del producto es " + resultado);
+		System.out.println("Esto es un cambio desde Eclipse en su WorkSpace "
+				+ "dentro de mi repositorio LOCAL sincronizado con Github.com");
+		
 	}
 
 }
