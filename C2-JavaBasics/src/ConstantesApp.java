8e87dd3797454cc8f28174497a005a5951703f47
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-02-14 11:07:44 +0100

    Commits de variables constantes

---
+++ C2-JavaBasics/src/ConstantesApp.java
@@ -0,0 +1,12 @@
+
+public class ConstantesApp {
+
+	public static void main(String[] args) {
+		final double IVA = 0.18;
+		int producto = 300;
+//		IVA = 0.21; A las Constantes no se les puede reasignar valor
+		double resultado = producto * IVA;
+		System.out.println("El IVA del producto es " + resultado);
+	}
+
+}
---
+++ C2-JavaBasics/src/Prueba2App.java
@@ -0,0 +1,11 @@
+
+public class Prueba2App {
+
+	public static void main(String[] args) {
+		int a = 30, b = 60, c = 20, suma;
+		suma = a + b + c;
+		System.out.println("La suma de las tres variables es: " + suma);
+
+	}
+
+}
