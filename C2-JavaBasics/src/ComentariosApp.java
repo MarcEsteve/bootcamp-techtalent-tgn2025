ce1ec32f566de1a54d302d31a2df3e678ed44517
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-02-14 09:41:56 +0100

    Comentarios y eclipse en el repo

---
+++ C2-JavaBasics/src/ComentariosApp.java
@@ -0,0 +1,16 @@
+
+public class ComentariosApp {
+
+	public static void main(String[] args) {
+		/*
+		 * Comenatario multilinea
+		 */
+		
+//		slkdfnjksdfsdjk
+		System.out.println("Hola Mundo");
+		System.out.println("Bienvenidos y bienvenidas");
+//		Ctrl + 7 para comentar o descomentar
+
+	}
+
+}
