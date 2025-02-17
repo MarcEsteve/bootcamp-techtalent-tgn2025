8c76a737191975f934bd4599b4ebb4433859c53c
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-02-17 10:45:55 +0100

    Create OpComparacionRelacionApp.java

---
+++ C2-JavaBasics/src/OpComparacionRelacionApp.java
@@ -0,0 +1,25 @@
+
+public class OpComparacionRelacionApp {
+
+	public static void main(String[] args) {
+		int op1 = 10, op2 = 20, op3 = 30;
+		boolean result = false;
+		
+		result = (op1 == op2);
+//		System.out.println(result);
+		result = (op1 != op2);
+//		System.out.println(result);
+		
+//		Operadores lógicos "condicionales" AND && y OR ||
+		result = (op1 >3 && op2<15);
+		System.out.println(result);
+		
+		
+//		EXTRA
+//		Ejemplo de importancia en algunos casos del paréntesis
+//		System.out.println("El resultado de la suma es " + (op1 + op2));
+		
+
+	}
+
+}
