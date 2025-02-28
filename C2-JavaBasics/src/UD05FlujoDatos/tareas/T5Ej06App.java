6c7c3cc3634e96deaae2d4b72f90bf6e0f3913fc
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-02-28 10:19:32 +0100

    Update T5Ej06App.java

--- C2-JavaBasics/src/UD05FlujoDatos/tareas/T5Ej06App.java
+++ C2-JavaBasics/src/UD05FlujoDatos/tareas/T5Ej06App.java
@@ -6,12 +6,16 @@ public class T5Ej06App {
 
 	public static void main(String[] args) {
 		final double IVA = 0.21; // 21%
+		double IRPF = 0.15;
 
+//		IVA= 0.22;
+		IRPF = 0.18;
 		double precioBase = Double.parseDouble(JOptionPane.showInputDialog(""
 				+ "Introduce un precio de base: "));
 		double precio = precioBase + precioBase * IVA;
 
-		JOptionPane.showMessageDialog(null, "" + "El precio final es: " + precio);
+		JOptionPane.showMessageDialog(null, "" + "El precio final es: " 
+		+ precio);
 
 	}
 
