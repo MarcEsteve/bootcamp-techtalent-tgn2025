bc3e6bdb52912bdd29a2a5d1054dbab37be1cef6
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-03-06 09:02:42 +0100

    Más tareas

---
+++ C2-JavaBasics/src/UD05FlujoDatos/tareas/T5Ej11App.java
@@ -0,0 +1,31 @@
+package UD05FlujoDatos.tareas;
+
+import javax.swing.JOptionPane;
+
+public class T5Ej11App {
+
+	public static void main(String[] args) {
+		String semana = ""; // Inicializaciones en String
+
+		semana = JOptionPane.showInputDialog("" + "Introduce el dia de la semana: ");
+
+		semana = semana.toLowerCase();
+
+		String laborableMensaje = "";
+
+		switch (semana) {
+			case "lunes", "martes", "miércoles", "jueves", "viernes":
+				laborableMensaje = "Laborable";
+				break;
+			default:
+				laborableMensaje = "No laborable " + " o no es un dia de la semana";
+	//			default:
+	//				laborableMensaje ="No hay valor o " 
+	//						+ "no es un dia de la semana";
+				break;
+		}
+		System.out.println(laborableMensaje);
+
+	}
+
+}
---
+++ C2-JavaBasics/src/UD05FlujoDatos/tareas/T5Ej12App.java
@@ -0,0 +1,33 @@
+package UD05FlujoDatos.tareas;
+
+import javax.swing.JOptionPane;
+
+public class T5Ej12App {
+
+	public static void main(String[] args) {
+		String pass = "12345", passUsuario = "", mensaje = "";
+		int intentos = 0;
+		
+		while(intentos<3) {
+			
+			passUsuario=JOptionPane.
+					 showInputDialog("Introduce el pass: ");
+			if (pass.equals(passUsuario)) {
+				mensaje = "Enhorabuena, login correcto";
+				break;
+			} else {
+				JOptionPane.showMessageDialog(null, "No es una contraseña correcta, "
+												+ "vuelve a probar");
+//				System.out.println("No es una contraseña correcta, vuelve a probar");
+				intentos++;
+			}
+			if(intentos == 3) {
+				mensaje = "Agotados el número de intentos";
+			}
+		}
+		
+//		System.out.println(mensaje);
+		JOptionPane.showMessageDialog(null, mensaje);
+	}
+
+}
