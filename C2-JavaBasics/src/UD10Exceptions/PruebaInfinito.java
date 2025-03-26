3c2ea0c47a1762da054f773fe40c13e32122e03c
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-03-26 10:32:11 +0100

    Create PruebaInfinito.java

---
+++ C2-JavaBasics/src/UD10Exceptions/PruebaInfinito.java
@@ -0,0 +1,23 @@
+package UD10Exceptions;
+
+public class PruebaInfinito {
+
+	public static void main(String[] args) {
+		int div = 0;
+
+//		int a = 12, b = 0;
+//		div = a / b;
+
+		try {
+			int a = 12, b = 0;
+			div = a / b;
+		} catch (Exception e) {
+			System.out.println("No puedes dividir por zero!!!");
+			System.out.println(e.getMessage());
+		}
+
+		System.out.println("El resultado es " + div);
+
+	}
+
+}
