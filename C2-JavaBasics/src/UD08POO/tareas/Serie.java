7e953a43438498b3b63cc3d7ddd80b97d400b071
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-04-07 10:52:49 +0200

    Create Serie.java

---
+++ C2-JavaBasics/src/UD08POO/tareas/Serie.java
@@ -0,0 +1,91 @@
+package UD08POO.tareas;
+
+public class Serie {
+	// Atributos
+	private String titulo;
+	private int nTemporadas;
+	private boolean entregado;
+	private String genero;
+	private String creador;
+
+	private final int TEMPORADAS_DEF = 3;
+	private final boolean ENTREGADO_DEF = false;
+
+	// Constructores
+	public Serie() {
+		this.titulo = "";
+		this.nTemporadas = TEMPORADAS_DEF;
+		this.entregado = ENTREGADO_DEF;
+		this.genero = "";
+		this.creador = "";
+	}
+
+	public Serie(String nombre, String director) {
+		this(); // En este caso hace referencia a nTemporadas, entregado y genero Serie()
+		// Equivalente
+//		this.nTemporadas = TEMPORADAS_DEF;
+//		this.entregado = ENTREGADO_DEF;
+//		this.genero = "";
+		this.titulo = nombre;
+		this.creador = director;
+	}
+	// new Serie("Juego de Tronos", "David Benioff");
+
+	public Serie(String nombre, int temp, String gendre, String director) {
+		this();
+//         this.entregado = false;//Equivalente
+		this.titulo = nombre;
+		this.nTemporadas = temp;
+		this.genero = gendre;
+		this.creador = director;
+
+	}
+    // Métodos getters y setters
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public int getnTemporadas() {
+		return nTemporadas;
+	}
+
+	public void setnTemporadas(int nTemporadas) {
+		this.nTemporadas = nTemporadas;
+	}
+
+	public boolean isEntregado() {
+		return entregado;
+	}
+
+	public void setEntregado(boolean entregado) {
+		this.entregado = entregado;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getCreador() {
+		return creador;
+	}
+
+	public void setCreador(String creador) {
+		this.creador = creador;
+	}
+
+	public int getTEMPORADAS_DEF() {
+		return TEMPORADAS_DEF;
+	}
+
+	public boolean isENTREGADO_DEF() {
+		return ENTREGADO_DEF;
+	}
+}
