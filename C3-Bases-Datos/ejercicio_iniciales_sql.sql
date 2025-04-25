b6f579f3816bc21ac13115620127f25447777e54
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-04-25 10:01:45 +0200

    Más sql y ejercicios

---
+++ C3-Bases-Datos/ejercicio_iniciales_sql.sql
@@ -0,0 +1,14 @@
+-- 1. Realizar una consulta que muestre los campos “Empresa” y “Población” de la tabla "Clientes". 
+SELECT * FROM clientes;
+
+SELECT EMPRESA,POBLACIÓN FROM clientes;
+-- 2. Realizar una consulta que muestre los artículos de la sección “Cerámica”. 
+SELECT NOMBREARTÍCULO AS `ARTÍCULOS DE CERÁMICA` FROM PRODUCTOS WHERE SECCIÓN='CERÁMICA';
+
+-- 3. Realizar una consulta que muestre los productos de la sección “Deportes” cuyo precio esté entre 100 y 200 €. 
+-- En la consulta solo se mostrarán los campos “Nombre de artículo” y “Precio”. 
+
+SELECT NOMBREARTÍCULO AS `DEPORTES`, PRECIO AS `ENTRE 100 y 200€` FROM PRODUCTOS WHERE SECCIÓN="DEPORTES" AND PRECIO BETWEEN 100 AND 200;
+
+-- 4. Realizar una consulta que muestre los productos cuyo país no sea España. 
+
