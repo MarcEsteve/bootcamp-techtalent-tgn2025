fe7b14fe6fc54eac27d612c1ad23cae03062a23f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-04-25 10:41:04 +0200

    Update ejercicio_iniciales_sql.sql

--- C3-Bases-Datos/ejercicio_iniciales_sql.sql
+++ C3-Bases-Datos/ejercicio_iniciales_sql.sql
@@ -12,8 +12,26 @@ SELECT NOMBREARTÍCULO AS `DEPORTES`, PRECIO AS `ENTRE 100 y 200€` FROM PRODUC
 
 -- 4. Realizar una consulta que muestre los productos cuyo país no sea España. 
 
+SELECT * FROM PRODUCTOS WHERE PAÍSDEORIGEN <> "ESPAÑA";
+SELECT * FROM PRODUCTOS WHERE IMPORTADO = "TRUE";
+SELECT * FROM PRODUCTOS WHERE PAÍSDEORIGEN IS NOT 'ESPAÑA';
+SELECT * FROM PRODUCTOS WHERE PAÍSDEORIGEN != 'ESPAÑA';
+
+-- 5. Realizar una consulta que muestre los artículos españoles de la sección “Deportes” o aquellos cuyo precio sea superior a 350 € independientemente de cual sea su sección o país de origen. 
+
 SELECT * 
-FROM productos 
+FROM PRODUCTOS 
 WHERE (SECCIÓN="DEPORTES" AND PAÍSDEORIGEN = "ESPAÑA")
 OR 
 PRECIO > 350;
+
+
+-- 6. Realizar una consulta que muestre los productos cuya fecha esté entre 1/05/2001 y 15/12/2001. En la consulta solo se visualizarán los campos “Nombre de artículo”, “Sección” y “Fecha”. 
+-- Modificamos el campo FECHA para tipo 'date'
+ALTER TABLE `productos` CHANGE `FECHA` `FECHA` DATE NULL DEFAULT NULL;
+
+-- FORMATO FECHA: '2001-05-01'
+
+SELECT NOMBREARTÍCULO, SECCIÓN, FECHA 
+FROM PRODUCTOS 
+WHERE FECHA BETWEEN '2001-05-01' AND '2001-12-15';
