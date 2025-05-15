f6ebab34bcfae1e46c466662087fa5f9bab64220
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-05-15 10:51:40 +0200

    Update EjemploCRUD02.java

--- C2-JavaBasics/src/UD18JDBC/EjemploCRUD02.java
+++ C2-JavaBasics/src/UD18JDBC/EjemploCRUD02.java
@@ -21,24 +21,24 @@ public class EjemploCRUD02 {
             statement = conexion.createStatement();
 
             // Insertar 3 registros en la tabla 'estacion'
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('41.5','1.15', 69)");
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('41.4','2.17', 12)");  // Barcelona
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('40.4','-3.7', 667)"); // Madrid
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('39.5','-0.37', 15)"); // Valencia
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('43.26','-2.93', 19)"); // Bilbao
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('36.7','-4.45', 8)");  // Málaga
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('42.8','-1.64', 450)"); // Pamplona
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('38.3','-0.48', 85)");  // Alicante
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('37.9','-1.12', 53)");  // Murcia
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('41.6','-0.88', 199)"); // Zaragoza
-//            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('43.4','-8.4', 0)");     // A Coruña
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('41.5','1.15', 69)");
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('41.4','2.17', 12)");  // Barcelona
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('40.4','-3.7', 667)"); // Madrid
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('39.5','-0.37', 15)"); // Valencia
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('43.26','-2.93', 19)"); // Bilbao
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('36.7','-4.45', 8)");  // Málaga
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('42.8','-1.64', 450)"); // Pamplona
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('38.3','-0.48', 85)");  // Alicante
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('37.9','-1.12', 53)");  // Murcia
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('41.6','-0.88', 199)"); // Zaragoza
+            statement.executeUpdate("INSERT INTO estacion (lat, longitud, alt) VALUES ('43.4','-8.4', 0)");     // A Coruña
 
 
             // Actualizar el nombre de un registro en la tabla 'estacion'
-//            statement.executeUpdate("UPDATE estacion SET lat = '41.3' WHERE id = 1");
+            statement.executeUpdate("UPDATE estacion SET lat = '41.3' WHERE id = 1");
 
             // Eliminar un registro específico de la tabla 'estacion'
-//            statement.executeUpdate("DELETE FROM estacion WHERE id = 2");
+            statement.executeUpdate("DELETE FROM estacion WHERE id = 2");
 
             // Consultar y mostrar los registros de la tabla 'fabricantes'
             ResultSet resultSet = statement.executeQuery("SELECT * FROM estacion");
