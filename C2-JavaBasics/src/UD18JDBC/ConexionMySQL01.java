eddef0103ae913a462ec702bd0400927bc114e6f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-05-15 10:20:51 +0200

    CRUD de Ejmplo

--- C2-JavaBasics/src/UD18JDBC/ConexionMySQL01.java
+++ C2-JavaBasics/src/UD18JDBC/ConexionMySQL01.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexionMySQL {
+public class ConexionMySQL01 {
 
 	public static void main(String[] args) {
 
