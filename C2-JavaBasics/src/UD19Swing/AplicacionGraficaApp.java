5300e20cf6ff064e6734a34875b670ec662aa986
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-05-16 10:58:22 +0200

    Aplicacion Gráfica SWING

---
+++ C2-JavaBasics/src/UD19Swing/AplicacionGraficaApp.java
@@ -0,0 +1,10 @@
+package UD19Swing;
+
+public class AplicacionGraficaApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AplicacionGrafica ventana =  new AplicacionGrafica();
+	}
+
+}
