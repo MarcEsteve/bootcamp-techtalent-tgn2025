3c0c509d4db4f6ffc1793eca89dff41183847874
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-06-27 09:38:43 +0200

    funciones asíncronas

---
+++ C5-FronEnd/JS/asincronas.js
@@ -0,0 +1,7 @@
+setTimeout(() => {
+    console.log('Esta instrucción se ejecuta después de 3 segundos, una sola vez');
+}, 3000);
+
+setInterval(() => {
+    console.log('Esta instrucción se ejecuta cada 4 segundos');
+}, 4000);
