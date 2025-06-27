488d7b8320a5f0e4a14d30417f290380866d724f
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-06-27 11:06:12 +0200

    más de DOM con JavaScript

---
+++ C5-FronEnd/JS/getElementsByTagName.js
@@ -0,0 +1,5 @@
+// Selecciona todos los elementos <h2>
+let subtitles = document.getElementsByTagName('h2');
+for (let i = 0; i < subtitles.length; i++) {
+  subtitles[i].style.color = 'blue';
+}
---
+++ C5-FronEnd/JS/querySelector.js
@@ -0,0 +1,4 @@
+// Selecciona el primer elemento <p> dentro de .container
+let paragraph = document.querySelector('.container .text');
+paragraph.style.fontWeight = 'bold';
+// Propiedad en CSS: font-weight: bold;
