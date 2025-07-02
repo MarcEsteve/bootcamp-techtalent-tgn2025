8f7bc39c61561c1293cae1efa542405a65bfb3a0
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-07-03 01:23:41 +0200

    Update 04-desestr-arrays.js

--- C5-FronEnd/REACT/01-inicial/src/basico/04-desestr-arrays.js
+++ C5-FronEnd/REACT/01-inicial/src/basico/04-desestr-arrays.js
@@ -1,7 +1,17 @@
+//Desstructuración de arrays en JavaScript
+
 // Lista de personajes de Spiderman
 const personajes = ['Peter Parker', 'Miles Morales', 'Gwen Stacy'];
 
+//Normal:
+
+console.log(personajes[0]); // Peter Parker
+console.log(personajes[1]); // Miles Morales
+console.log(personajes[2]); // Gwen Stacy
+
 // Queremos el tercer personaje
+const [personaje1 ] = personajes;
+const [ , personaje2 ] = personajes;
 const [ , , personaje3 ] = personajes;
 console.log(personaje3); // Gwen Stacy
 
@@ -10,8 +20,10 @@ const retornaSpideyInfo = () => {
     return ['Spidey', 2002];
 }
 
+// Desestructuración del array retornado
 const [ alias, añoAparicion ] = retornaSpideyInfo();
 console.log(alias, añoAparicion); // Spidey 2002
+
 
 // Simulación de un hook* tipo useState (como en React)
 const useSpiderState = (valor) => {
@@ -21,6 +33,11 @@ const useSpiderState = (valor) => {
 //Un hook* en React es una función que permite usar el "estado" y otras características de React sin escribir una clase.
 // *Nota: Aunque aquí no estamos en un entorno de React, usamos el término "hook" para ilustrar el concepto de estado y funciones asociadas.
 // "Estado" en React se refiere a la capacidad de un componente para mantener y gestionar datos que pueden cambiar con el tiempo, permitiendo que la interfaz de usuario se actualice automáticamente cuando esos datos cambian.
+
+// const arr = useSpiderState('Peter Parker');
+// console.log(arr);
+// arr[0]; // Peter Parker
+// arr[1](); // Lanzando telaraña desde Peter Parker
 
 // Desestructuración del array retornado
 const [ spiderNombre, lanzarTelaraña ] = useSpiderState('Peter Parker');
