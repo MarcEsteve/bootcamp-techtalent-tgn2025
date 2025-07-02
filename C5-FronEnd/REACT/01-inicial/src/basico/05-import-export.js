5f147050b091db6eef71a5cfdcbc50c5b5f3e3f5
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-07-03 01:43:57 +0200

    import exports

--- C5-FronEnd/REACT/01-inicial/src/basico/05-import-export.js
+++ C5-FronEnd/REACT/01-inicial/src/basico/05-import-export.js
@@ -1,16 +1,37 @@
-// import { heroes } from './data/heroes';
-// import { heroes } from './data/heroes';
-// import heroes, { owners } from '../data/heroes';
-import heroes from "../data/heroes";
+//Importaciones en JavaScript son una forma de incluir código de otros archivos o módulos en tu archivo actual. Esto es especialmente útil para organizar el código y reutilizar funciones, objetos o variables.
+//import {} from ''
 
+// import { heroes } from './data/heroes';
+// console.log( heroes );
+
+// import heroes, { owners } from '../data/heroes';
 // console.log( owners );
 
-export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+import heroes from "../data/heroes"; //
+
+// https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+
+// const getHeroeById = (id) =>{
+//   return heroes.find((heroe) => {
+//     if (heroe.id === id) {
+//       return true;
+      
+//     } else {
+//       return false;
+//     }
+//   });
+// }
 
 // console.log( getHeroeById(2) );
 
-// find?, filter
+
+export const getHeroeById = (id) => heroes.find((heroe) => heroe.id === id);
+
+console.log( getHeroeById(2) );
+
+
+// find? solo devuelve 1, filter todos
 export const getHeroesByOwner = (owner) =>
   heroes.filter((heroe) => heroe.owner === owner);
 
-// console.log( getHeroesByOwner('Marvel') );
+console.log( getHeroesByOwner('Marvel') );
