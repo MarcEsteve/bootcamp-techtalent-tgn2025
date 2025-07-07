5614a3ddec420d74f87c8c11272c73ce1edae587
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-07-07 07:28:22 +0200

    app de contador + componentes

--- C5-FronEnd/REACT/01-inicial/src/basico/10-async-await.js
+++ C5-FronEnd/REACT/01-inicial/src/basico/10-async-await.js
@@ -1,6 +1,9 @@
 // const getImagenPromesa = () => new Promise( resolve => resolve('https://cualquierdominio.com') )
 // getImagenPromesa().then( console.log );
 
+// Para hacer una petición a la API de Giphy y obtener un GIF aleatorio, utilizamos la función fetch() que devuelve una promesa.
+//async se utiliza para manejar operaciones asíncronas de manera más sencilla, permitiendo escribir código que se asemeja al estilo síncrono.
+//await se utiliza para esperar a que una promesa se resuelva o se rechace, lo que permite manejar el resultado de la promesa de manera más directa y legible.
 const getImagen = async () => {
   try {
     const apiKey = "C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC";
@@ -17,6 +20,7 @@ const getImagen = async () => {
   } catch (error) {
     // manejo del error
     console.error(error);
+    // Puedes mostrar un mensaje al usuario o realizar alguna acción específica
   }
 };
 
