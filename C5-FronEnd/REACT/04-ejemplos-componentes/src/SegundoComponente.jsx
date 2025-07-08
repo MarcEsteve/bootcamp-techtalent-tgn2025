2ea81eb1313e6eb657764fab17ed40e4f243ac3b
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-07-08 10:18:58 +0200

    Componentes y elementos y variables a mostrar en React

---
+++ C5-FronEnd/REACT/04-ejemplos-componentes/src/SegundoComponente.jsx
@@ -0,0 +1,29 @@
+// import { Fragment } from "react";
+const nombre = 'Marc';
+const nuevoMensaje= {
+     mensaje:'Hola Mundo',
+     nombre: 'Marc'
+};
+const boleano = true;
+
+const SegundoComponente = () => {
+
+    // const nombre = 'Marc';
+
+    return (
+  
+      <>
+        <p>Segundo componente</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non maxime inventore, nemo dolorum autem omnis. {nombre} Dolorem, magni eligendi beatae sapiente sunt molestias! Cum fuga quibusdam quam quidem distinctio vel fugit!</p>
+        <p>{ 1+2 }</p>
+        <p>{ nombre }</p>
+        {/* <p>{nuevoMensaje}</p> */}
+        <p>{ nuevoMensaje.mensaje }</p>
+        <p>{boleano}</p>
+        <code>{ JSON.stringify(nuevoMensaje) }</code>
+      </>
+
+    )
+}
+
+export default SegundoComponente;
--- C5-FronEnd/REACT/04-ejemplos-componentes/src/main.jsx
+++ C5-FronEnd/REACT/04-ejemplos-componentes/src/main.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App';
+import SegundoComponente from './SegundoComponente';
 
-//Función "Componente"
-function App() {
-  //document.createElement...
-  return (<h1>Hola mundo</h1>);
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
     <App/>
-    
+    <SegundoComponente/>
   </React.StrictMode>
 );
