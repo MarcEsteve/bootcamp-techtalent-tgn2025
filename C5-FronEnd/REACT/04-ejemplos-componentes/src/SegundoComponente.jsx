506a85230c789e4060eae73567efdc5a7d1f1a0e
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-07-09 10:52:42 +0200

    Ejemplo condicional con dos componentes

--- C5-FronEnd/REACT/04-ejemplos-componentes/src/SegundoComponente.jsx
+++ C5-FronEnd/REACT/04-ejemplos-componentes/src/SegundoComponente.jsx
@@ -1,37 +1,49 @@
 // import { Fragment } from "react";
-import Mar from './assets/mar1.avif';
-import MiBoton from './Button';
-import Perfil from './Perfil';
+import Mar from "./assets/mar1.avif";
+import MiBoton from "./Button";
+import Perfil from "./Perfil";
+import Shakira from "./Shakira";
+import Topuria from "./Topuria";
 
-const nombre = 'Marc';
-const nuevoMensaje= {
-     mensaje:'Hola Mundo',
-     nombre: 'Marc'
+const nombre = "Marc";
+const nuevoMensaje = {
+  mensaje: "Hola Mundo",
+  nombre: "Marc",
 };
-const boleano = true;
+const boleano = false;
+
+let content;
+if (boleano) {
+  content = <Shakira />;
+} else {
+  content = <Topuria />;
+}
 
 const SegundoComponente = () => {
+  // const nombre = 'Marc';
 
-    // const nombre = 'Marc';
-
-    return (
-  
-      <>
-        <p>Segundo componente</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non maxime inventore, nemo dolorum autem omn pepito is. {nombre} Dolorem, magni eligendi beatae sapiente sunt molestias! Cum fuga quibusdam quam quidem distinctio vel fugit!</p>
-        <p>{ 1+2 }</p>
-        <MiBoton />
-        <button>Otro botón</button>
-        <Perfil />
-        <p>{ nombre }</p>
-        {/* <p>{nuevoMensaje}</p> */}
-        <p>{ nuevoMensaje.mensaje }</p>
-        <p>{boleano}</p>
-        <code>{ JSON.stringify(nuevoMensaje) }</code>
-        <img src={Mar} alt="Una imagen del pepito" />
-      </>
-
-    )
-}
+  return (
+    <>
+      <p>Segundo componente</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non maxime
+        inventore, nemo dolorum autem omn pepito is. {nombre} Dolorem, magni
+        eligendi beatae sapiente sunt molestias! Cum fuga quibusdam quam quidem
+        distinctio vel fugit!
+      </p>
+      <p>{1 + 2}</p>
+      <MiBoton />
+      <button>Otro botón</button>
+      <div>{content}</div>
+      {/* <Perfil /> */}
+      <p>{nombre}</p>
+      {/* <p>{nuevoMensaje}</p> */}
+      <p>{nuevoMensaje.mensaje}</p>
+      <p>{boleano}</p>
+      <code>{JSON.stringify(nuevoMensaje)}</code>
+      <img src={Mar} alt="Una imagen del pepito" />
+    </>
+  );
+};
 
 export default SegundoComponente;
