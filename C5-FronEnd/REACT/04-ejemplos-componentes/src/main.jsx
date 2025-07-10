28f7f2dafcaf038810a88831a95d83649fa2330a
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2025-07-10 09:39:36 +0200

    render de arrays con map()

--- C5-FronEnd/REACT/04-ejemplos-componentes/src/main.jsx
+++ C5-FronEnd/REACT/04-ejemplos-componentes/src/main.jsx
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App';
 import SegundoComponente from './SegundoComponente';
 import ComponenteProp from './ComponenteProp';
+import ShoppingList from './Arrays';
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
-    <SegundoComponente/>
-    <ComponenteProp title="Hola soy Marc" subtitulo={123} />
+    {/* <App/> */}
+    {/* <SegundoComponente/> */}
+    {/* <ComponenteProp title="Hola soy Marc" subtitulo={123} /> */}
+    <ShoppingList />
   </React.StrictMode>
 );
